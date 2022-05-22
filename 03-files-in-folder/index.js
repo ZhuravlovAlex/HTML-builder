a466257076cab145107d0063daa992c7045ec1f7
ZhuravlovAlex <zhuravlov.alex75@gmail.com>
2022-05-22 18:09:46 +0300

    feat: add task #4

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,19 +3,31 @@
 const fs = require('fs');
 const path = require('path');
 
+const folder = path.join(__dirname,  'secret-folder');
+
 //Чтение папки
-fs.readdir("../secret-folder", (err, files) => {
+fs.readdir(folder, (err, files) => {
 	if (err) throw err;
-	return files;
+	files.map(file => {
+		const stats = fs.stat(path.join(folder, file), (err, stats) => {
+			if (stats.isDirectory()) return;
+
+			const size = stats.size;
+			
+			const extension = path.extname(path.join(folder, file)).substring(1);
+
+			console.log(`${file}	- ${extension} - ${size} bytes`);
+		})
+	})
 });
 
 
 // чтение инфо из файла
-fs.readFile(
-	path.join(__dirname, 'notes', 'mynotes.txt'),
-	'utf-8',
-	(err, data) => {
-		if (err) throw err;
-		console.log(data);
-	}
-);
+// fs.readFile(
+// 	path.join(__dirname, 'notes', 'mynotes.txt'),
+// 	'utf-8',
+// 	(err, data) => {
+// 		if (err) throw err;
+// 		console.log(data);
+// 	}
+// );
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+
+const source = path.join(__dirname, 'files');
+const target = path.join(__dirname, 'files-copy');
+
+//  Создание папки, если ее нет
+// Проверка наличия папки
+fs.stat(target, function(err) {
+	if (err && err.code === 'ENOENT') {
+		fs.mkdir(target, err => {
+   		if(err) throw err; // не удалось создать папку
+			console.log('Папка успешно создана');
+		});
+	}
+
+
+	fs.readdir(target, (err, files) => {
+		files.forEach(file => {
+			fs.unlink(path.join(target, file), () => {})
+		});
+		fs.readdir(source, (err, files) => {
+		if(err) throw err; 
+		files.forEach(file => {
+			fs.copyFile(path.join(source, file), path.join(target, file), err => {
+				if(err) throw err; // не удалось скопировать файл
+				console.log('Файл успешно скопирован');
+			});
+		})
+	});
+	})
+	
+});
+
+
+
+
+
 
